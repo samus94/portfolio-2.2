ce1422b0190eda12cb8c9495b81da8501008e76a
samus94 <spatmwin@live.com>
2021-01-12 19:27:23 +0800

    moving index

--- src/Main.js
+++ src/Main.js
@@ -1,28 +1,37 @@
-import { Component } from 'react' 
-import './Main.css'
+import React, { Component } from "react";
+import {
+Route,
+NavLink,
+HashRouter
+} from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
+// import Resume from "./Resume"
+
+
 
 class Main extends Component {
-    render() {
-        return(
-            <div className="ImgBox">
-                <div className="tidBitDiv">
-                    <div className="tidBitOne">
-                        <h1 className="tidOneHead">Full Stack </h1>
-                        <p className="tidOneP"> 
-                            Web developter, designing applications for mobile phones and desktop using CSS, HTML, Node, React, and Ruby. 
-                        </p>
-                    </div>
-                    <div className="tidBitTwo">
-                        <h1 className="tidTwoHead">Aspiring Game Designer</h1>
-                        <p className="tidTwoP">
-                            My goal is to put the skills I've learned as a full-stack web developer and bring it to the gaming industry by becoming a full-fledged game developer. As a gamer of many years, I have learned from game designs that have succeeded, and more imporantly those that have failed. 
-                        </p>
-                    </div>
+  render() {
+    return (
+        <HashRouter>
+            <div>
+                <ul className="header">
+                    <li><NavLink exact to="/">Home</NavLink></li>
+                    <li><NavLink to="/about">About</NavLink></li>
+                    <li><NavLink to="/contact">Contact</NavLink></li>
+                    {/* <li><NavLink to="/resume">Resume</NavLink></li> */}
+                </ul>
+                <div className="content">
+                    <Route exact path="/" component={Home}/>
+                    <Route path="/about" component={About}/>
+                    <Route path="/contact" component={Contact}/>
+                    {/* <Route path="/resume" component={Resume}/> */}
                 </div>
-
             </div>
-        )
-    }
+            </HashRouter>
+    );
+  }
 }
-
-export default Main
+ 
+export default Main;
--- src/Resume.js
+++ src/Resume.js
@@ -1,15 +1,21 @@
-import React, { Component } from 'react'
-import SamCVfinal from './SamCVfinal.png'
 
-class Resume extends Component {
-    render () {
-        return (
-            <div>
-                <h1>Resume</h1>
-                <img src={SamCVfinal} className="project-image" ></img>
-            </div>
-        )
-    }
-}
-
-export default Resume;
+// import React, { Component, useState } from "react";
+// import { Document } from 'react-pdf';
+// // import MyDocument from "./pdf.js"
+// import myCV from './myCV.pdf'
+// import ReactDom from 'react-dom';
+// // import {PDFViewer} from '@react-pdf/renderer'
+// // import ReactPDF from '@react-pdf/renderer';
+// // import MyApp from './pdf.js'
+// class Resume extends Component {
+//   render() {
+    
+//     return (
+//       <div>
+//         {/* <MyApp/> */}
+//       </div>
+//     );
+//   }
+// }
+ 
+// export default Resume;
